47be5d07b2a5919d3d9dd76b83e82328bf961f4d
Shahriar <mdfshahriar0@gmail.com>
2026-05-19 13:11:24 +0300

    Add files via upload

---
+++ Agent/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine(
+                "Usage: Agent.exe <name> <target>");
+            Console.ReadLine();
+            return;
+        }
+
+        string agentName = args[0];
+
+        int targetNumber = int.Parse(args[1]);
+
+        Console.WriteLine(
+            "Agent " + agentName + " started.");
+
+        Random random = new Random();
+
+        while (true)
+        {
+            // Slow down guessing
+            await Task.Delay(10);
+
+            int guess = random.Next(1, 1001);
+
+            // REMOVE SPAM OUTPUT
+            // Console.WriteLine(
+            //     "Agent " + agentName +
+            //     " guessed " + guess);
+
+            if (guess == targetNumber)
+            {
+                using (NamedPipeClientStream pipe =
+                    new NamedPipeClientStream(
+                        ".",
+                        "GuessPipe",
+                        PipeDirection.Out))
+                {
+                    await pipe.ConnectAsync();
+
+                    using (StreamWriter writer =
+                        new StreamWriter(pipe))
+                    {
+                        writer.AutoFlush = true;
+
+                        string message =
+                            "Agent " + agentName +
+                            " guessed the number " +
+                            targetNumber;
+
+                        await writer.WriteLineAsync(
+                            message);
+
+                        Console.WriteLine(message);
+                    }
+                }
+
+                break;
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
